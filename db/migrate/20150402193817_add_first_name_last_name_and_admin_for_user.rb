72ede16b8862a3c96d9affcd4c63e13ce59477ef
Tomasz <tarnawski@go2.pl>
2015-04-02 22:21:57 +0200

    Add first name, last name and field admin to user and modify Devise bundle

---
+++ db/migrate/20150402193817_add_first_name_last_name_and_admin_for_user.rb
@@ -0,0 +1,7 @@
+class AddFirstNameLastNameAndAdminForUser < ActiveRecord::Migration
+  def change
+   add_column :users, :firstname, :string
+   add_column :users, :lastname, :string
+   add_column :users, :admin, :boolean, :default => false
+  end
+end
