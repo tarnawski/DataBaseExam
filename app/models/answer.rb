7fde0038e18e0a8fde5868dd20f7f77ebec8a19f
Tomasz <tarnawski@go2.pl>
2015-04-04 15:24:21 +0200

    Before add Result

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,30 @@
 class Answer < ActiveRecord::Base
 belongs_to :student
 
+def self.mark(test_id, points)
 
+    current_test = Test.find(test_id)
+
+    if points>=current_test.bdb
+    return 5;
+    elsif points>=current_test.bd
+    return 4;
+    elsif points>=current_test.dost
+    return 3;
+    elsif points<current_test.dost
+    return 2;
+    end
 end
+
+def self.points(current_student_id)
+    points = 0
+    user_answer = Answer.where(student_id: current_student_id).all
+    user_answer.each do |answer|
+    good_answer = Question.find(answer.question_id)
+    if(answer.answer_a == good_answer.a_is_correct && answer.answer_b == good_answer.b_is_correct && answer.answer_c ==     good_answer.c_is_correct && answer.answer_d == good_answer.d_is_correct)
+    points = points + 1;
+    end
+end
+end
+end
+
---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :user do
+    firstname 'John'
+    lastname 'Doe'
+    email 'nickname@o2.pl'
+    password 'password123'
+  end
+end
